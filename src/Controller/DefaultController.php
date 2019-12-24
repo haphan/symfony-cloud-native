be1036ebfeb005f46e86c2e3ad95bff8d24a4893
haphan <thanhha.phan@myrepublic.net>
2019-12-24 10:38:55 +0800

    Hello symfony 5.0

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,10 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class DefaultController extends AbstractController
 {
     public function index(Request $request)
     {
